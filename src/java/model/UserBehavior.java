64e3787a1db4268ba9d3db0886edd0fadc916de6
Your Name <you@example.com>
2019-02-19 11:44:57 +0100

    Class create

---
+++ src/java/model/UserBehavior.java
@@ -0,0 +1,23 @@
+package model;
+
+
+import model.Interface.IBehavior;
+
+import java.util.ArrayList;
+
+public class UserBehavior implements IBehavior {
+    @Override
+    public void placeSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries, int soldiersToPlace) {
+
+    }
+
+    @Override
+    public void attackCountry(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries) {
+
+    }
+
+    @Override
+    public void moveSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries) {
+
+    }
+}
