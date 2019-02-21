780e207e4fb2038f813d918464a0589337828948
crnjatin <crnjatin@students.zhaw.ch>
2019-02-21 18:34:25 +0100

    Create Class Aggressive behavior

---
+++ src/java/model/AggressiveBehavior.java
@@ -0,0 +1,24 @@
+package model;
+
+import model.Interface.IBehavior;
+
+import java.util.ArrayList;
+
+public class AggressiveBehavior implements IBehavior {
+
+
+    @Override
+    public void placeSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries, int soldiersToPlace) {
+
+    }
+
+    @Override
+    public void attackCountry(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries) {
+
+    }
+
+    @Override
+    public void moveSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries) {
+
+    }
+}
