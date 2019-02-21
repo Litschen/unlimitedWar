27f7b3656306ba98c4bc941878000f534c975f5a
crnjatin <crnjatin@students.zhaw.ch>
2019-02-21 18:22:35 +0100

    Add computerPlayers

--- src/java/model/UserBehavior.java
+++ src/java/model/UserBehavior.java
@@ -6,6 +6,18 @@ import model.Interface.IBehavior;
 import java.util.ArrayList;
 
 public class UserBehavior implements IBehavior {
+
+    /**
+     * Creates Player for the game
+     * @param allCountries
+     * @param ownedCountries
+     * @param soldiersToPlace
+     */
+
+    Player computerPlayer01;
+    Player computerPlayer02;
+    Player computerPlayer03;
+
     @Override
     public void placeSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries, int soldiersToPlace) {
 
