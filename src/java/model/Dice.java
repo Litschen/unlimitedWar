726e173ad8917f46f18a33ae4825a511b2d38650
schrema8 <schrema8@students.zhaw.ch>
2019-02-22 14:00:28 +0100

    -Cleared up dice.roll signature and javadoc

--- src/java/model/Dice.java
+++ src/java/model/Dice.java
@@ -2,8 +2,14 @@ package model;
 
 public class Dice {
 
-    public int rollTheDice(int wert) {
+    /**
+     *
+     * @param amountOfDice how many dice should be thrown
+     * @return int array of results (range 1-6) sorted by by size descending
+     */
+    public int[] roll(int amountOfDice) {
 
-       return  wert = (int) (Math.random() * 6 + 1);
+       //return  wert = (int) (Math.random() * 6 + 1);
+        return new int[]{-1};
     }
 }
