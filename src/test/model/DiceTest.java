ce9867a9a15d29e49743561c85b2cc815f564828
huguemiz <huguemiz@students.zhaw.ch>
2019-02-23 16:06:23 +0100

    use List instead of Array

--- src/test/model/DiceTest.java
+++ src/test/model/DiceTest.java
@@ -2,17 +2,19 @@ package model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DiceTest {
 
-    private int[] diceResults;
+    private List<Integer> diceResults;
 
     @Test
     void rollDefault() {
         diceResults = new Dice().roll(4);
-        assertEquals(diceResults.length, Dice.MAX_THROWS - 1);
+        assertEquals(diceResults.size(), Dice.MAX_THROWS - 1);
         int previousResult = Dice.MAX_VALUE;
         for(int result : diceResults){
             assertTrue(result >= Dice.MIN_VALUE && result <= Dice.MAX_VALUE);
@@ -24,12 +26,12 @@ class DiceTest {
     @Test
     void rollNegative(){
         diceResults = new Dice().roll(-3);
-        assertEquals(diceResults.length, 0);
+        assertEquals(diceResults.size(), 0);
     }
 
     @Test
     void rollMax(){
         diceResults = new Dice().roll(Integer.MAX_VALUE);
-        assertEquals(diceResults.length,0);
+        assertEquals(diceResults.size(),0);
     }
 }
