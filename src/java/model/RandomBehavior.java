4ab660e0441a0d6eedba27fd368325af73fc0a85
crnjatin <crnjatin@students.zhaw.ch>
2019-02-23 19:13:32 +0100

    implements method moveSoldiers (correct one)

--- src/java/model/RandomBehavior.java
+++ src/java/model/RandomBehavior.java
@@ -46,8 +46,13 @@ public class RandomBehavior implements IBehavior {
      * @param ownedCountries by current Player
      */
     @Override
-    public void moveSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries) {
-        //TODO
+    public void moveSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries, int soldiersToPlace) {
+
+        while (soldiersToPlace > 1){
+            placeSoldiers(allCountries, ownedCountries, soldiersToPlace);
+        }
+
+
     }
 
 
