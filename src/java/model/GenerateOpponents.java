976ba1a486be0c66702d3a8cba3bf188b8e7c8dd
crnjatin <crnjatin@students.zhaw.ch>
2019-02-23 19:27:48 +0100

    Create computer opponents

--- src/java/model/GenerateOpponents.java
+++ src/java/model/GenerateOpponents.java
@@ -3,7 +3,9 @@ package java.model;
 
 public class GenerateOpponents {
 
-
+    model.AggressiveBehavior  aggressiveBehavior = new model.AggressiveBehavior ();
+    model.UserBehavior userBehavior = new model.UserBehavior();
+    model.RandomBehavior randomBehavior = new model.RandomBehavior();
 
 
 
