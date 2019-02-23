91dec8c100b6eb38b30983afd5de38204158ffde
crnjatin <crnjatin@students.zhaw.ch>
2019-02-23 19:28:47 +0100

    Make a comment

--- src/java/model/GenerateOpponents.java
+++ src/java/model/GenerateOpponents.java
@@ -3,6 +3,8 @@ package java.model;
 
 public class GenerateOpponents {
 
+    //bJust ignore model. ... My ID does not recognize the package
+
     model.AggressiveBehavior  aggressiveBehavior = new model.AggressiveBehavior ();
     model.UserBehavior userBehavior = new model.UserBehavior();
     model.RandomBehavior randomBehavior = new model.RandomBehavior();
