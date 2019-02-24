aae7fb46ce5999554b064c850e5449e79e610651
huguemiz <huguemiz@students.zhaw.ch>
2019-02-24 15:38:59 +0100

    cleaned up PlayerTest  - renamed method

--- src/test/model/PlayerTest.java
+++ src/test/model/PlayerTest.java
@@ -12,23 +12,23 @@ class PlayerTest {
     private Player testPlayer;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         testPlayer = new Player("blue", "testPlayer", new RandomBehavior());
     }
 
     @Test
-    void calculateSoldiersToPlaceTestMin() {
-        //owned countries = 0
-        assertEquals(Player.COUNTRY_WEIGHT, testPlayer.calculateSoldiersToPlace());
-    }
-
-    @Test
-    void calculateSoldiersToPlaceTest() {
+    void testCalculateSoldiersToPlace() {
         ArrayList<Country> owned = testPlayer.getOwnedCountries();
-        for(int i = 0; i < 15 ; i++){
+        for (int i = 0; i < 15; i++) {
             owned.add(new Country("test", 1, testPlayer));
         }
         assertEquals(15 / Player.COUNTRY_WEIGHT, testPlayer.calculateSoldiersToPlace());
+    }
+
+    @Test
+    void testCalculateSoldiersToPlaceMin() {
+        //owned countries = 0
+        assertEquals(Player.COUNTRY_WEIGHT, testPlayer.calculateSoldiersToPlace());
     }
 
 }
