3cb47a755dca2b3292e2a1abb5ed0df8c5544919
crnjatin <crnjatin@students.zhaw.ch>
2019-02-25 23:38:44 +0100

    Add Javadoc in class AggressiveBehavior

--- src/java/model/AggressiveBehavior.java
+++ src/java/model/AggressiveBehavior.java
@@ -7,16 +7,29 @@ import java.util.ArrayList;
 public class AggressiveBehavior implements IBehavior {
 
 
+    /**
+     * @param allCountries
+     * @param ownedCountries
+     * @param soldiersToPlace
+     */
     @Override
     public void placeSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries, int soldiersToPlace) {
 
     }
 
+    /**
+     * @param allCountries
+     * @param ownedCountries
+     */
     @Override
     public void attackCountry(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries) {
 
     }
 
+    /**
+     * @param allCountries
+     * @param ownedCountries
+     */
     @Override
     public void moveSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries) {
 
