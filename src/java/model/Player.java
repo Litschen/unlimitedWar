6f39bc0024fffb7a9196874e9853e2b549dcc19d
crnjatin <crnjatin@students.zhaw.ch>
2019-02-26 00:00:21 +0100

    Add Javadoc in class Player

--- src/java/model/Player.java
+++ src/java/model/Player.java
@@ -22,6 +22,11 @@ public class Player{
     private ArrayList<Country> ownedCountries;
     //endregion
 
+    /**Constructor. Create Player with color, name and behavior
+     * @param color
+     * @param name
+     * @param behavior
+     */
     //region constructor
     public Player(String color, String name, IBehavior behavior) {
         this.color = color;
@@ -31,23 +36,38 @@ public class Player{
     }
     //endregion
 
+    /**
+     * @return by Player color
+     */
     //region getter setter
     public String getPlayerColor() {
         return color;
     }
 
+    /**
+     * @return by Player name
+     */
     public String getPlayerName() {
         return name;
     }
 
+    /**
+     * @return by get Behavior
+     */
     public IBehavior getBehavior(){
         return behavior;
     }
 
+    /**
+     * @return by behavior own countries
+     */
     public ArrayList<Country> getOwnedCountries() { return ownedCountries; }
     //endregion
 
 
+    /**
+     * @return by number of soldiers to place
+     */
     public int calculateSoldiersToPlace() {
         return Math.max(COUNTRY_WEIGHT, getOwnedCountries().size() / COUNTRY_WEIGHT);
     }
