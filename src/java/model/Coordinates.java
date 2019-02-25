3836039144045bd57247d732d2912ca8f73e4419
crnjatin <crnjatin@students.zhaw.ch>
2019-02-25 23:53:39 +0100

    Add Javadoc in class Coordinates

--- src/java/model/Coordinates.java
+++ src/java/model/Coordinates.java
@@ -8,6 +8,12 @@ public class Coordinates {
     private int right;
     //endregion
 
+    /** Set Coordinates
+     * @param top
+     * @param right
+     * @param bottom
+     * @param left
+     */
     //region Constructor
     public Coordinates(int top, int right, int bottom, int left) {
         this.top = top;
@@ -17,19 +23,31 @@ public class Coordinates {
     }
     //endregion
 
+    /**
+     * @return by top
+     */
     //region getter setter
     public int getTop() {
         return top;
     }
 
+    /**
+     * @return by bottom
+     */
     public int getBottom() {
         return bottom;
     }
 
+    /**
+     * @return by left
+     */
     public int getLeft() {
         return left;
     }
 
+    /**
+     * @return by Right
+     */
     public int getRight() {
         return right;
     }
