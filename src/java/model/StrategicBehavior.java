d04587e235cba6f88e999ce82441971f9aa041b2
crnjatin <crnjatin@students.zhaw.ch>
2019-02-25 23:28:01 +0100

    Create Class StrategicBehavior

---
+++ src/java/model/StrategicBehavior.java
@@ -0,0 +1,22 @@
+package model;
+
+import model.Interface.IBehavior;
+
+import java.util.ArrayList;
+
+public class StrategicBehavior implements IBehavior {
+    @Override
+    public void placeSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries, int soldiersToPlace) {
+
+    }
+
+    @Override
+    public void attackCountry(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries) {
+
+    }
+
+    @Override
+    public void moveSoldiers(ArrayList<Country> allCountries, ArrayList<Country> ownedCountries) {
+
+    }
+}
