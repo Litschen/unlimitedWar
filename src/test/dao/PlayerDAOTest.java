771c274bc8f1262db06edc088439f8fa142a4448
huguemiz <huguemiz@students.zhaw.ch>
2019-02-26 10:13:45 +0100

    add Test class for PlayerDAO

---
+++ src/test/dao/PlayerDAOTest.java
@@ -0,0 +1,18 @@
+package dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PlayerDAOTest {
+    private PlayerDAO playerDAO;
+
+    @BeforeEach
+    public void setUp() {
+        playerDAO = new PlayerDAO();
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
