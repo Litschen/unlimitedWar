e0a97e45151be42dfe608c9faaee5d7b1781ee70
crnjatin <crnjatin@students.zhaw.ch>
2019-03-02 17:05:28 +0100

    Create ENUM ColorPlayer and random selection color

---
+++ src/java/model/Enum/ColorPlayer.java
@@ -0,0 +1,17 @@
+package model.Enum;
+
+import java.util.Random;
+
+public enum ColorPlayer {
+
+    RED, BLUE, GREEN, YELLOW;
+
+    private static final ColorPlayer[] VALUES = values();
+    private static final int SIZE = VALUES.length;
+    private static final Random RANDOM = new Random();
+
+    public static ColorPlayer getRandomLetter()  {
+        return VALUES[RANDOM.nextInt(SIZE)];
+    }
+
+}
