3fbdc242e2428edd882c1c01ccae8b0c81d71ef4
huguemiz <huguemiz@students.zhaw.ch>
2019-03-07 17:13:02 +0100

    test Dice.java

--- src/java/model/Dice.java
+++ src/java/model/Dice.java
@@ -10,11 +10,11 @@ public class Dice {
     //region static variables
     public static final int MAX_VALUE = 6;
     public static final int MIN_VALUE = 1;
-    public static final int MAX_THROWS = 10;
     //endregion
 
-
     /**
+     * throw a dice (range 1-6)
+     *
      * @return Random number between 1 and 6
      */
     public static int roll() {
@@ -22,8 +22,11 @@ public class Dice {
     }
 
     /**
+     * throw a given amount of dice (range 1-6) and save the results in an array.
+     * the results are sorted in descending order.
+     *
      * @param amountOfDice how many dice should be thrown
-     * @return int array of results (range 1-6) sorted by by size descending
+     * @return int array of results sorted descending
      */
     public static int[] roll(int amountOfDice) {
         List<Integer> dices = new ArrayList<>();
@@ -39,14 +42,27 @@ public class Dice {
     }
 
     /**
-     * @param min
-     * @param max
-     * @return number between min and max
+     * generate a random number between the passed min and max value (inclusive)
+     *
+     * @param min minimum value of the result
+     * @param max maximum value of the result
+     * @return generated number
      */
     public static int roll(int min, int max) {
+        if (min > max) {
+            int tmp = min;
+            min = max;
+            max = tmp;
+        }
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
+    /**
+     * convert a List of Integer to an int array
+     *
+     * @param list list of integers
+     * @return int[] with all elements of the passed list
+     */
     private static int[] toIntArray(List<Integer> list) {
         int[] array = new int[list.size()];
         for (int i = 0; i < array.length; i++) {
