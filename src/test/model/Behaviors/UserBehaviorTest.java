66d166bc0676321216652a12d2cfbf763e7cdccf
crnjatin <crnjatin@students.zhaw.ch>
2019-03-11 23:02:44 +0100

    implement Test: void testPlaceSoldiers()

--- src/test/model/Behaviors/UserBehaviorTest.java
+++ src/test/model/Behaviors/UserBehaviorTest.java
@@ -1,62 +1,57 @@
 package model.Behaviors;
 
 import model.Country;
+import model.CountryTest;
+import model.Enum.Phase;
+import model.Enum.PlayerColor;
 import model.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import model.Enum.Phase;
-
-import static junit.framework.TestCase.assertNotNull;
 import static model.Enum.PlayerColor.BLUE;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 class UserBehaviorTest {
 
-    private Player testplayer;
-    private Country testCountry;
+    private Player testPlayer;
     private ArrayList<Country> selectedCountries;
     private ArrayList<Country> ownedCountries;
 
 
-        @BeforeEach
-        public void setUp () {
-            testplayer = new Player(BLUE, "Jackob", new UserBehavior());
-            selectedCountries = new ArrayList<>();
-            ownedCountries = new ArrayList<>();
-            }
-// Aufrug hier generieren
-
-
-    @Test
-    void testSelectedCountries() {
-
-        assertNotNull(selectedCountries); // schuat ob nivht null
-        assertEquals(0, selectedCountries.size());
+    @BeforeEach
+    public void setUp() {
+        testPlayer = new Player(BLUE, "Jackob", new UserBehavior());
+        ownedCountries = new ArrayList<>();
+        selectedCountries = new ArrayList<>();
     }
-
-    @Test
-    void testOwnedCountries() {
-        assertNotNull(selectedCountries);
-        assertNotNull("List shouldn't be null", ownedCountries);
-        assertEquals(0, ownedCountries.size());
-        assertEquals("Wrong 1st element", "Customer1", ownedCountries.get(0));
-        assertEquals("Wrong 2nd element", "Customer2", ownedCountries.get(1));
-        assertEquals("Wrong 3rd element", "Customer3", ownedCountries.get(2));
-        assertEquals("Wrong 3rd element", "Customer3", ownedCountries.get(3));
-    }
-
 
     @Test
     void testPlaceSoldiers() {
 
-        assertEquals(Phase.SETTINGPHASE, testUserBehavior.placeSoldiers(selectedCountries, ownedCountries, 3));
-        assertEquals(Phase.SETTINGPHASE, testUserBehavior.placeSoldiers(selectedCountries, ownedCountries, 2));
-        assertEquals(Phase.SETTINGPHASE, testUserBehavior.placeSoldiers(selectedCountries, ownedCountries, 3));
-        assertEquals(Phase.ATTACKPHASE, testUserBehavior.placeSoldiers(selectedCountries, ownedCountries, 0));
+        selectedCountries = CountryTest.makeList(1, testPlayer);
+        ownedCountries = CountryTest.makeList(4, testPlayer);
+        ownedCountries.add(selectedCountries.get(0));
 
+        testPlayer.setSoldiersToPlace(3);
+
+        assertEquals(Phase.SETTINGPHASE, testPlayer.getBehavior().placeSoldiers(selectedCountries, ownedCountries, 0));
+        assertEquals(6, selectedCountries.get(0).getSoldiersCount());
+        assertEquals(2, testPlayer.getSoldiersToPlace());
+
+        assertEquals(Phase.SETTINGPHASE, testPlayer.getBehavior().placeSoldiers(selectedCountries, ownedCountries, 0));
+        assertEquals(7, selectedCountries.get(0).getSoldiersCount());
+        assertEquals(1, testPlayer.getSoldiersToPlace());
+
+        assertEquals(Phase.ATTACKPHASE, testPlayer.getBehavior().placeSoldiers(selectedCountries, ownedCountries, 0));
+        assertEquals(8, selectedCountries.get(0).getSoldiersCount());
+        assertEquals(0, testPlayer.getSoldiersToPlace());
+
+        assertEquals(Phase.ATTACKPHASE, testPlayer.getBehavior().placeSoldiers(selectedCountries, ownedCountries, 0));
+        assertEquals(8, selectedCountries.get(0).getSoldiersCount());
+        assertEquals(0, testPlayer.getSoldiersToPlace());
     }
 
     @Test
