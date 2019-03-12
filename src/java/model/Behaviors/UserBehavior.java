974e0a3235c824773893a7b02cccd7f4847a4ad7
schrema8 <schrema8@students.zhaw.ch>
2019-03-12 14:05:06 +0100

    -cleanup userbehavior for codereview, cleared up javadoc

--- src/java/model/Behaviors/UserBehavior.java
+++ src/java/model/Behaviors/UserBehavior.java
@@ -10,24 +10,20 @@ import java.util.ArrayList;
 public class UserBehavior implements IBehavior {
 
     /**
-     * New soldiers have to be placed on their own countries.
-     * Put one soldiers on one selected country. This is repeated until it has no longer soldiers to place
+     * Puts one soldier on the destinationCountries[0] country. This is repeated until there are no soldiers to place
      *
-     * @param destinationCountries
-     * @param ownedCountries       are countries from current player
-     * @param soldiersToPlace      it would never be used
-     * @return phase set phase as far as it can be to set by soldiers. Or phase attack phase none solders are to put on
+     * @param destinationCountries on index 0 is the select country to place 1 soldier
+     * @param ownedCountries       countries from current player
+     * @param soldiersToPlace      unused
+     * @return phase set as long as soldierstoplace of the owner is greater than 0 otherwise attack phase
      */
     @Override
     public Phase placeSoldiers(ArrayList<Country> destinationCountries, ArrayList<Country> ownedCountries, int soldiersToPlace) {
         Phase phase = Phase.SETTINGPHASE;
         Country destination = destinationCountries.get(0);
         Player owner = destination.getOwner();
-
         if (owner.getSoldiersToPlace() > 0) {
-
             if (ownedCountries.contains(destination)) {
-
                 destination.setSoldiersCount(destination.getSoldiersCount() + 1);
                 owner.setSoldiersToPlace(owner.getSoldiersToPlace() - 1);
                 if (owner.getSoldiersToPlace() == 0) {
@@ -37,19 +33,15 @@ public class UserBehavior implements IBehavior {
         } else {
             phase = Phase.ATTACKPHASE;
         }
-
-
         return phase;
     }
 
     /**
-     * The player selects a country and a neighboring country to attack.
-     * Only those countries with more than 1 soldier can attack
+     * Attacks selectedCountries[1] from selectedCountries[0].
      *
-     * @param selectedCountries ist an ArrayList from selected countries. Index 0 is the country, witch is been attacked
-     *                          and index 1 it's the country witch has been defended.
-     * @param ownedCountries    are owned by the player
-     * @return replace the next phase manually
+     * @param selectedCountries [0] attacks [1]
+     * @param ownedCountries    of current player
+     * @return attackphase, next phase is set from controller.
      */
     @Override
     public Phase attackCountry(ArrayList<Country> selectedCountries, ArrayList<Country> ownedCountries) {
@@ -61,17 +53,15 @@ public class UserBehavior implements IBehavior {
             int defendDiceCount = defendCountry.amountDiceThrowsDefender(attackDiceCount);
             attackCountry.invade(defendCountry, attackDiceCount, defendDiceCount);
         }
-
         return Phase.ATTACKPHASE;
     }
 
     /**
-     * The soldiers are singular being moved from one own country to another own country.
-     * The countries must both be on the same continent. Only those countries with more than 1 soldier can move
+     * Moves 1 soliders from selectedCountries[0] to selectedCountries[1] if possible
      *
      * @param selectedCountries are countries the player has selected
-     * @param ownedCountries    are countries from current player
-     * @return replace the next phase  manually
+     * @param ownedCountries    all countries from current player
+     * @return setting phase, next phase set in controller
      */
     @Override
     public Phase moveSoldiers(ArrayList<Country> selectedCountries, ArrayList<Country> ownedCountries) {
