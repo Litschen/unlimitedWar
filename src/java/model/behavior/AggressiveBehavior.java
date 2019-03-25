80930a6197d0694b6d07e15b85ed255744fde296
huguemiz <huguemiz@students.zhaw.ch>
2019-03-25 16:05:55 +0100

    #27 implemented set phase of aggressive player

--- src/java/model/behavior/AggressiveBehavior.java
+++ src/java/model/behavior/AggressiveBehavior.java
@@ -2,11 +2,14 @@ package model.behavior;
 
 import model.AttackCountryResult;
 import model.Country;
+import model.Player;
 import model.enums.Phase;
 import model.interfaces.Behavior;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * TODO in MS2
@@ -25,7 +28,57 @@ public class AggressiveBehavior implements Behavior {
      */
     @Override
     public Phase placeSoldiers(List<Country> allCountries, List<Country> ownedCountries, int soldiersToPlace) {
+        if (ownedCountries.size() > 0) {
+            List<Country> countriesToSet = mostEffectiveCountry(ownedCountries, ownedCountries.get(0).getOwner());
+
+            for (int i = 0; soldiersToPlace > 0 && countriesToSet.size() > 0; soldiersToPlace--) {
+                Country c = countriesToSet.get(i);
+                c.setSoldiersCount(c.getSoldiersCount() + 1);
+                i++;
+                if (i >= countriesToSet.size()) {
+                    i = 0;
+                }
+            }
+        }
         return Phase.ATTACKPHASE;
+    }
+
+    /**
+     * return a list of countries to set a soldier
+     */
+    private List<Country> mostEffectiveCountry(List<Country> invadeFromCountries, Player owner) {
+        Map<Country, Integer> map = new HashMap<>();
+        for (Country invadeFrom : invadeFromCountries) {
+            List<Country> invadeCountry = new ArrayList<>();
+
+            // check how far the user can invade from this country
+            checkIfAbleToAttackFurther(invadeCountry, invadeFrom, owner);
+            map.put(invadeFrom, invadeCountry.size());
+        }
+
+        // evaluate the most effective country / countries to attack from
+        List<Country> maxEntry = new ArrayList<>();
+        int max = 0;
+        for (Map.Entry<Country, Integer> entry : map.entrySet()) {
+            if (entry.getValue() >= max) {
+                if (entry.getValue() > max) {
+                    maxEntry.clear();
+                }
+                maxEntry.add(entry.getKey());
+                max = entry.getValue();
+            }
+        }
+
+        return maxEntry;
+    }
+
+    private void checkIfAbleToAttackFurther(List<Country> invadeCoutnry, Country invadeFrom, Player owner) {
+        for (Country neighbor : invadeFrom.getNeighboringCountries()) {
+            if (!invadeCoutnry.contains(neighbor) && !neighbor.getOwner().equals(owner)) {
+                invadeCoutnry.add(neighbor);
+                checkIfAbleToAttackFurther(invadeCoutnry, neighbor, owner);
+            }
+        }
     }
 
     /**
@@ -74,6 +127,7 @@ public class AggressiveBehavior implements Behavior {
 
     /**
      * Checks if there are any countries that can be invaded
+     *
      * @param ownedCountries by player of this behavior
      * @return ArrayList of countries which can invade another.
      */
@@ -82,7 +136,7 @@ public class AggressiveBehavior implements Behavior {
         for (Country country : ownedCountries) {
             int i = 0;
             List<Country> neighbors = country.getNeighboringCountries();
-            while (i < neighbors.size() && !country.canInvade(neighbors.get(i)) ) {
+            while (i < neighbors.size() && !country.canInvade(neighbors.get(i))) {
                 i++;
             }
             if (i < neighbors.size()) {
