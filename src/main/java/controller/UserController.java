0c5412bdd379182c53c57c8b35975eb446f3a836
Manuel Schreier <schrema8@students.zhaw.ch>
2019-04-18 16:45:00 +0200

    -closes #4 added testcases for (un)succesfull signin, made playerdao datafield for test purposes, added controller, renamed displayErrorMessage to DISPLAY_ERROR_MESSAGE

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -20,43 +20,55 @@ import java.util.logging.Logger;
 public class UserController extends HttpServlet {
 
     //region static variables
-    private final static Logger LOGGER = Logger.getLogger(UserController.class.getName());
-    private final static String PAGE_TO_LOAD_ON_COMPLETE = "/jsp/index.jsp";
-    private final static String PAGE_TO_LOAD_ON_ERROR = "/jsp/sign-in.jsp";
+    public final static String PAGE_TO_LOAD_ON_COMPLETE = "/jsp/index.jsp";
+    public final static String PAGE_TO_LOAD_ON_ERROR = "/jsp/sign-in.jsp";
     public final static String SIGNIN_ERROR_MESSAGE = "Could not sign in. Maybe you entered your password or Email wrong";
     public final static String MAIL_PARAMETER_NAME = "mail";
     public final static String PASSWORD_PARAMETER_NAME = "password";
-    public static boolean displayErrorMessage = false;
+    private final static Logger LOGGER = Logger.getLogger(UserController.class.getName());
+    public static boolean DISPLAY_ERROR_MESSAGE = false;
     //endregion
 
     //region data fields
     private UserBean user = null;
+    private PlayerDAO playerDAO;
     //endregion
+
+    public UserController() {
+        try {
+            playerDAO = new PlayerDAO(MySQLConnectionCreator.getConnection());
+        } catch (ClassNotFoundException | SQLException e) {
+            LOGGER.log(Level.WARNING, "DATABASE ERROR: Could not establish connection", e);
+        }
+    }
 
     //region getter/setter
     public UserBean getUser() {
         return user;
     }
-
     public void setUser(UserBean user) {
         this.user = user;
     }
+
+    //added for testing purposes
+    public PlayerDAO getPlayerDAO() {
+        return this.playerDAO;
+    }
     //endregion
+
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
         String mail = request.getParameter(MAIL_PARAMETER_NAME);
         String password = request.getParameter(PASSWORD_PARAMETER_NAME);
 
-        try {
-            PlayerDAO playerDAO = new PlayerDAO(MySQLConnectionCreator.getConnection());
-            user = playerDAO.getValidatedUser(mail, password);
-        } catch (ClassNotFoundException | SQLException e) {
-            LOGGER.log(Level.WARNING, "DATABASE ERROR: Could not establish connection", e);
+        if (getPlayerDAO() != null) {
+            user = getPlayerDAO().getValidatedUser(mail, password);
         }
+
         RequestDispatcher requestDispatcher = (user != null) ?
                 request.getRequestDispatcher(PAGE_TO_LOAD_ON_COMPLETE) : request.getRequestDispatcher(PAGE_TO_LOAD_ON_ERROR);
-        displayErrorMessage = user == null;
+        DISPLAY_ERROR_MESSAGE = user == null;
         try {
             requestDispatcher.forward(request, response);
         } catch (ServletException | IOException e) {
---
+++ src/main/test/controller/UserControllerTest.java
@@ -0,0 +1,73 @@
+package controller;
+
+import dao.PlayerDAO;
+import model.UserBean;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+class UserControllerTest {
+
+    //region static variables
+    private final static String VALID_EMAIL = "max@gmail.com";
+    private final static String VALID_PASSWORD = "123";
+    private final static String INVALID_EMAIL = "asdfsajk";
+    private final static String INVALID_PASSWORD = "asdfasdf";
+    //endregion
+    //region data fields
+    private HttpServletRequest mockRequest;
+    private HttpServletResponse mockResponse;
+    private UserController controller = spy(new controller.UserController());
+    private PlayerDAO mockPlayerDao;
+    //endregion
+
+    @BeforeEach
+    void setUp() {
+        mockPlayerDao = mock(PlayerDAO.class);
+        when(controller.getPlayerDAO()).thenReturn(mockPlayerDao);
+        mockRequest = mock(HttpServletRequest.class);
+        mockResponse = mock(HttpServletResponse.class);
+        when(mockRequest.getRequestDispatcher(UserController.PAGE_TO_LOAD_ON_ERROR))
+                .thenReturn(mock(RequestDispatcher.class));
+        when(mockRequest.getRequestDispatcher(UserController.PAGE_TO_LOAD_ON_COMPLETE))
+                .thenReturn(mock(RequestDispatcher.class));
+    }
+
+    @Test
+    void testSuccessfulSignIn() {
+        //setup valid parameter extraction
+        when(mockRequest.getParameter(UserController.MAIL_PARAMETER_NAME)).thenReturn(VALID_EMAIL);
+        when(mockRequest.getParameter(UserController.PASSWORD_PARAMETER_NAME)).thenReturn(VALID_PASSWORD);
+
+        //setup dao validation
+        when(mockPlayerDao.getValidatedUser(VALID_EMAIL, VALID_PASSWORD)).thenReturn(new UserBean());
+
+        controller.doPost(mockRequest, mockResponse);
+
+        assertFalse(UserController.DISPLAY_ERROR_MESSAGE);
+        verify(mockRequest, times(1)).getRequestDispatcher(UserController.PAGE_TO_LOAD_ON_COMPLETE);
+
+    }
+
+    @Test
+    void testUnsuccessfulSignIn() {
+        //setup invalid parameter extraction
+        when(mockRequest.getParameter(UserController.MAIL_PARAMETER_NAME)).thenReturn(INVALID_EMAIL);
+        when(mockRequest.getParameter(UserController.PASSWORD_PARAMETER_NAME)).thenReturn(INVALID_PASSWORD);
+
+        //setup dao validation
+        when(mockPlayerDao.getValidatedUser(INVALID_EMAIL, INVALID_PASSWORD)).thenReturn(null);
+
+        controller.doPost(mockRequest, mockResponse);
+
+        assertTrue(UserController.DISPLAY_ERROR_MESSAGE);
+        verify(mockRequest, times(1)).getRequestDispatcher(UserController.PAGE_TO_LOAD_ON_ERROR);
+    }
+}
