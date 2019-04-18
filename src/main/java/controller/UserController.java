f592d2aa6bf5925ebd1f5f40836f2cc0919dbbd5
Manuel Schreier <schrema8@students.zhaw.ch>
2019-04-18 14:49:47 +0200

    -#4 created UserController with Log in functionality, currently untested, because I dont have sqlworkbench on this system

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,60 @@
+package controller;
+
+import dao.MySQLConnectionCreator;
+import dao.PlayerDAO;
+import model.UserBean;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+@WebServlet(name = "UserController", urlPatterns = "/SignIn")
+public class UserController extends HttpServlet {
+
+    public final static String MAIL_PARAMETER_NAME = "mail";
+    //endregion
+    public final static String PASSWORD_PARAMETER_NAME = "password";
+    //region static variables
+    private final static Logger LOGGER = Logger.getLogger(UserController.class.getName());
+    private final static String PAGE_TO_LOAD_ON_COMPLETE = "/jsp/index.jsp";
+    private final static String PAGE_TO_LOAD_ON_ERROR = "/jsp/sign-in.jsp";
+    //region data fields
+    private UserBean user = null;
+    //endregion
+
+    //region getter/setter
+    public UserBean getUser() {
+        return user;
+    }
+    //endregion
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+        String mail = request.getParameter(MAIL_PARAMETER_NAME);
+        String password = request.getParameter(PASSWORD_PARAMETER_NAME);
+
+        try {
+            PlayerDAO playerDAO = new PlayerDAO(MySQLConnectionCreator.getConnection());
+            user = playerDAO.getValidatedUser(mail, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            LOGGER.log(Level.WARNING, "DATABASE ERROR: Could not establish connection", e);
+        }
+        RequestDispatcher requestDispatcher = (user != null) ?
+                request.getRequestDispatcher(PAGE_TO_LOAD_ON_COMPLETE) : request.getRequestDispatcher(PAGE_TO_LOAD_ON_ERROR);
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException | IOException e) {
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+        }
+    }
+
+}
