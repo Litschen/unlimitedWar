c3876be0587c5e75e8c45fb61b9dec7525e25fe3
crnjatin <crnjatin@students.zhaw.ch>
2019-04-22 19:14:59 +0200

    remove unnecessary variable

--- src/main/java/dao/ResultsDAO.java
+++ src/main/java/dao/ResultsDAO.java
@@ -7,7 +7,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.*;
-import java.util.logging.Logger;
 
 public class ResultsDAO {
     private ResultSet rs;
