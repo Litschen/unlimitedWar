18a1cd530c549129ff7e7497abb1b786707e0e2d
crnjatin <crnjatin@students.zhaw.ch>
2019-04-26 00:12:33 +0200

    #52 try to write method hash password
    - hash works. However, the get method does not throw the hashed value
    
    will still be fixed

--- src/main/java/ch/zhaw/unlimitedWar/model/UserBean.java
+++ src/main/java/ch/zhaw/unlimitedWar/model/UserBean.java
@@ -1,6 +1,8 @@
 package ch.zhaw.unlimitedWar.model;
 
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 
 public class UserBean implements Serializable {
     private String name;
@@ -28,6 +30,24 @@ public class UserBean implements Serializable {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = getPasswordMD5Text(password);
+    }
+
+    private static String getPasswordMD5Text(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(password.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+            while (hashtext.length() < 23) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
