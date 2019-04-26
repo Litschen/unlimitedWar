88d0471408e794ffa5d33fdfa8af23e0fc22143d
huguemiz <huguemiz@students.zhaw.ch>
2019-04-26 23:07:47 +0200

    add session constants

--- src/main/java/ch/zhaw/unlimitedWar/controller/Pages.java
+++ src/main/java/ch/zhaw/unlimitedWar/controller/Pages.java
@@ -2,10 +2,16 @@ package ch.zhaw.unlimitedWar.controller;
 
 public class Pages {
 
+    // Pages
     public final static String GAME = "/jsp/game.jsp";
     public final static String HOME = "/jsp/home.jsp";
     public final static String PROFILE = "/jsp/profile.jsp";
     public final static String RESULTS = "/jsp/results.jsp";
     public final static String SIGN_IN = "/jsp/sign-in.jsp";
+
+    // Session
+    public final static String SESSION_USER = "user";
+    public final static String SESSION_EVENTS = "events";
+    public final static String SESSION_BOARD = "board";
 
 }
