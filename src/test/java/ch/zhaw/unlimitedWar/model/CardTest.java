b9986e58279c6a59fd49704b88ea9b28513ba6a0
huguemiz <huguemiz@students.zhaw.ch>
2019-04-29 14:50:23 +0200

    #30 add test to card

---
+++ src/test/java/ch/zhaw/unlimitedWar/model/CardTest.java
@@ -0,0 +1,62 @@
+package ch.zhaw.unlimitedWar.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class CardTest {
+
+    private Card card;
+    private Country mockCountry;
+
+    @BeforeEach
+    void setUp() {
+        mockCountry = mock(Country.class);
+        card = new Card(mockCountry, 2);
+    }
+
+    @Test
+    public void testGetCardName() {
+        when(mockCountry.getName()).thenReturn("name");
+
+        assertEquals("name", card.getCardName());
+        verify(mockCountry, times(1)).getName();
+    }
+
+    @Test
+    public void testCardBonus() {
+        Player player = mock(Player.class);
+        when(mockCountry.getOwner()).thenReturn(null);
+
+        assertEquals(2, card.getCardBonus(player));
+    }
+
+    @Test
+    public void testCardBonusWithOwningBonus() {
+        Player player = mock(Player.class);
+        when(mockCountry.getOwner()).thenReturn(player);
+
+        assertEquals(3, card.getCardBonus(player));
+    }
+
+    @Test
+    public void testIsPlayerOwner() {
+        Player player = mock(Player.class);
+        when(mockCountry.getOwner()).thenReturn(player);
+
+        assertTrue(card.isPlayerOwner(player));
+        verify(mockCountry, times(1)).getOwner();
+    }
+
+    @Test
+    public void testIsNotPlayerOwner() {
+        Player player = mock(Player.class);
+        when(mockCountry.getOwner()).thenReturn(null);
+
+        assertFalse(card.isPlayerOwner(player));
+        verify(mockCountry, times(1)).getOwner();
+    }
+
+}
