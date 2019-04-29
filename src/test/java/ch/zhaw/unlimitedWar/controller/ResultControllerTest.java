9775aa3ce74bb14946731d83cc7c39643c9261ac
crnjatin <crnjatin@students.zhaw.ch>
2019-04-29 14:01:53 +0200

    #36 setup() ResultControllerTest

--- src/test/java/ch/zhaw/unlimitedWar/controller/ResultControllerTest.java
+++ src/test/java/ch/zhaw/unlimitedWar/controller/ResultControllerTest.java
@@ -1,11 +1,38 @@
 package ch.zhaw.unlimitedWar.controller;
 
+import ch.zhaw.unlimitedWar.dao.ResultsDAO;
+import ch.zhaw.unlimitedWar.model.UserBean;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static org.mockito.Mockito.*;
 
 class ResultControllerTest {
 
-    @Test
-    void getResult() {
+    private ResultsDAO resultDAOmock;
+    private HttpServletRequest mockRequest;
+    private HttpServletResponse mockResponse;
+    private ResultController resultController;
+    private UserBean userBean;
+    private HttpSession httpSession;
+    private ResultController resultControllerspy;
+
+    @BeforeEach
+    void setUp() {
+        resultDAOmock = mock(ResultsDAO.class);
+        mockRequest = mock(HttpServletRequest.class);
+        when(mockRequest.getContextPath()).thenReturn("");
+        mockResponse = mock(HttpServletResponse.class);
+        when(mockRequest.getSession()).thenReturn(mock(HttpSession.class));
+        resultController = new ResultController();
+        userBean = mock(UserBean.class);
+        httpSession = mock(HttpSession.class);
+        resultControllerspy = spy(new ResultController());
+
     }
 
     @Test
@@ -16,6 +43,13 @@ class ResultControllerTest {
     void getResultDAO() {
     }
 
-    @Test
-    void testFinishGame() {}
+
+
+
+
+
+
+
+
+
 }
