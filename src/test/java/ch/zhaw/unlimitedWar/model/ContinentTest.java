f2158d7b1b1bbcb9a62d443479b8bf95a3457d50
schrema8 <schrema8@students.zhaw.ch>
2019-04-30 14:44:46 +0200

    -#13 implemented ContinentTest @huguemiz

---
+++ src/test/java/ch/zhaw/unlimitedWar/model/ContinentTest.java
@@ -0,0 +1,35 @@
+package ch.zhaw.unlimitedWar.model;
+
+import ch.zhaw.unlimitedWar.helpers.TestHelperBehavior;
+import ch.zhaw.unlimitedWar.model.behavior.RandomBehavior;
+import ch.zhaw.unlimitedWar.model.enums.PlayerColor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ContinentTest {
+
+    private Player player;
+    private Continent continent;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player(PlayerColor.BLUE, "", new RandomBehavior());
+        continent = new Continent(10, "");
+    }
+
+    @Test
+    void getTextColorPositiveTest() {
+        continent.addCountries(TestHelperBehavior.getCountryList(5, player));
+        assertEquals(player.getPlayerColor().toString(), continent.getTextColor());
+
+    }
+
+    @Test
+    void getTextColorNegativeTest() {
+        continent.addCountries(TestHelperBehavior.getCountryList(5, player));
+        continent.addCountries(TestHelperBehavior.getCountryList(5, TestHelperBehavior.getMockPlayer()));
+        assertEquals(Continent.STANDARD_TEXT_COLOR, continent.getTextColor());
+    }
+}
