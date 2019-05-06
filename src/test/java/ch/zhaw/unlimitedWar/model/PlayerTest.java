79d5ad3bf24dd5e9d96e8ac7e32234b3d6c6f9bc
schrema8 <schrema8@students.zhaw.ch>
2019-05-06 21:48:19 +0200

    -#55 renamed fellowCountries to createOwnedCountries

--- src/test/java/ch/zhaw/unlimitedWar/model/PlayerTest.java
+++ src/test/java/ch/zhaw/unlimitedWar/model/PlayerTest.java
@@ -26,35 +26,35 @@ class PlayerTest {
 
     @Test
     void testCalculateSoldiersToPlace() {
-        fellowCountries(testPlayer.getOwnedCountries(), 1);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 1);
         assertEquals(3, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 5);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 5);
         assertEquals(3, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 10);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 10);
         assertEquals(3, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 12);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 12);
         assertEquals(4, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 12);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 12);
         assertEquals(4, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 30);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 30);
         assertEquals(10, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
     }
 
     @Test
     void testCalculateSoldiersToPlaceContinentBonus() {
-        fellowCountries(testPlayer.getOwnedCountries(), 1);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 1);
         Continent continent = new Continent(10, testPlayer.getOwnedCountries(), "test");
         assertEquals(10 + Player.COUNTRY_WEIGHT, testPlayer.calculateSoldiersToPlace(Collections.singletonList(continent)));
     }
 
     @Test
     void testCalculateSoldiersToPlaceContinentNotOwned() {
-        fellowCountries(testPlayer.getOwnedCountries(), 1);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 1);
         Continent continent = new Continent(10, testPlayer.getOwnedCountries(), "test");
         continent.addCountry(TestHelperBehavior.getMockCountry(mock(Player.class)));
         assertEquals(Player.COUNTRY_WEIGHT, testPlayer.calculateSoldiersToPlace(Collections.singletonList(continent)));
@@ -68,36 +68,36 @@ class PlayerTest {
 
     @Test
     void testCalculateSoldiersToPlaceCapitalBonus() {
-        fellowCountries(testPlayer.getOwnedCountries(), 1, true, true);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 1, true, true);
         assertEquals(Player.COUNTRY_WEIGHT + 1, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 6, true, true);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 6, true, true);
         assertEquals(10, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 15, true, true);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 15, true, true);
         assertEquals(25, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
     }
 
     @Test
     void testCalculateSoldiersToPlaceCityBonus() {
-        fellowCountries(testPlayer.getOwnedCountries(), 6, true, false);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 6, true, false);
         assertEquals(4, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 7, true, false);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 7, true, false);
         assertEquals(4, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 8, true, false);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 8, true, false);
         assertEquals(5, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
 
-        fellowCountries(testPlayer.getOwnedCountries(), 15, true, false);
+        createOwnedCountries(testPlayer.getOwnedCountries(), 15, true, false);
         assertEquals(10, testPlayer.calculateSoldiersToPlace(new ArrayList<>()));
     }
 
-    private void fellowCountries(List<Country> owned, int amountOfCountries) {
-        fellowCountries(owned, amountOfCountries, false, false);
+    private void createOwnedCountries(List<Country> owned, int amountOfCountries) {
+        createOwnedCountries(owned, amountOfCountries, false, false);
     }
 
-    private void fellowCountries(List<Country> owned, int amountOfCountries, boolean city, boolean capital) {
+    private void createOwnedCountries(List<Country> owned, int amountOfCountries, boolean city, boolean capital) {
         owned.clear();
         for (int i = 0; i < amountOfCountries; i++) {
             Country country = new Country("test", 1, testPlayer);
